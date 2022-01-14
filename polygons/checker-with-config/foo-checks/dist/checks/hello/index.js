e54342cb2be6abb03fc635378f8f4e754e6354f9
Tangerine Cat <kaineer@gmail.com>
2022-01-14 13:00:08 +0500

    Добавляет проверку hello в foo-checks

--- polygons/checker-with-config/foo-checks/dist/checks/hello/index.js
+++ polygons/checker-with-config/foo-checks/dist/checks/hello/index.js
@@ -1,1 +1,27 @@
-const
+// dist/checks/hello/index.js
+
+const {
+  CommonHtmlCheck,
+  QueryMethods
+} = require("@htmlacademy/autochecks-skeleton");
+
+export default class HelloCheck extends CommonHtmlCheck {
+  getMessages() {
+    return {
+      title: "index.html should have charset meta",
+      success: "charset meta found",
+      failure: "charset meta is not found"
+    };
+  }
+
+  getHtmlChecks() {
+    const { createNotFoundCheck } = CommonHtmlCheck;
+
+    return [
+      createNotFoundCheck("meta[charset]", async () => ({
+        success: false,
+        title: "Отсутствует тег с кодировкой"
+      }))
+    ];
+  }
+}
